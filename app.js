784927a8d02ccf10d5ca9c25cd4f18288112c86b
trongvinhne <309741773+trongvinhne@users.noreply.github.com>
2026-07-29 20:44:41 +0700

    h

---
+++ app.js
@@ -0,0 +1,108 @@
+// ==========================================
+// VTP Check V4
+// app.js
+// ==========================================
+
+const videoInput = document.getElementById("videoInput");
+const video = document.getElementById("video");
+const scanBtn = document.getElementById("scanBtn");
+const result = document.getElementById("result");
+const progressBar = document.getElementById("progressBar");
+
+let selectedFile = null;
+
+// =========================
+// Chọn video
+// =========================
+
+videoInput.addEventListener("change", (e) => {
+
+    const file = e.target.files[0];
+
+    if (!file) return;
+
+    selectedFile = file;
+
+    video.src = URL.createObjectURL(file);
+
+    result.value =
+`✔ Đã chọn video
+
+Tên:
+${file.name}
+
+Dung lượng:
+${(file.size / 1024 / 1024).toFixed(2)} MB`;
+
+});
+
+// =========================
+// Bắt đầu quét
+// =========================
+
+scanBtn.addEventListener("click", async () => {
+
+    if (!selectedFile) {
+
+        alert("Hãy chọn video.");
+
+        return;
+
+    }
+
+    progressBar.style.width = "10%";
+
+    try {
+
+        await waitVideoReady();
+
+        progressBar.style.width = "100%";
+
+        result.value +=
+
+`\n
+----------------------
+
+Thời lượng:
+
+${video.duration.toFixed(2)} giây
+
+Độ phân giải:
+
+${video.videoWidth} × ${video.videoHeight}`;
+
+    }
+
+    catch (err) {
+
+        alert(err.message);
+
+    }
+
+});
+
+// =========================
+// Đợi video sẵn sàng
+// =========================
+
+function waitVideoReady() {
+
+    return new Promise((resolve) => {
+
+        if (video.readyState >= 1) {
+
+            resolve();
+
+            return;
+
+        }
+
+        video.onloadedmetadata = () => {
+
+            resolve();
+
+        };
+
+    });
+
+}
